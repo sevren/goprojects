2f158e566871d277a930a9c0b7bfd6813d5c7d3a
Kiril Tzvetanov Goguev <kiril.goguev@coesia.com>
2018-04-20 10:39:00 +0200

    Added structs with pointers

---
+++ structs/main.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
+type person struct {
+	firstName string
+	lastName  string
+	contactInfo
+}
+
+func (p person) print() {
+
+	fmt.Printf("%+v", p)
+
+}
+
+func (pointerToPerson *person) updateName(newFirstName string) {
+	(*pointerToPerson).firstName = newFirstName
+}
+
+func main() {
+
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Party",
+		contactInfo: contactInfo{
+			email:   "jim@gmail.com",
+			zipCode: 94000,
+		},
+	}
+
+	jim.updateName("jimmy")
+	jim.print()
+
+}
